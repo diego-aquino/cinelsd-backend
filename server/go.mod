4d2053d0eaf9b32d7deb5d0b044adc37b4ab5a63
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-05-14 15:31:26 -0300

    feat: add go server

---
+++ server/go.mod
@@ -0,0 +1,31 @@
+module cinelsd-backend-server
+
+go 1.20
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f
+	github.com/gin-contrib/sse v0.1.0
+	github.com/gin-gonic/gin v1.8.2
+	github.com/go-playground/locales v0.14.1
+	github.com/go-playground/universal-translator v0.18.1
+	github.com/go-playground/validator/v10 v10.11.2
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/goccy/go-json v0.10.0
+	github.com/json-iterator/go v1.1.12
+	github.com/leodido/go-urn v1.2.1
+	github.com/mattn/go-isatty v0.0.17
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
+	github.com/modern-go/reflect2 v1.0.2
+	github.com/pelletier/go-toml/v2 v2.0.6
+	github.com/redis/go-redis v6.15.9+incompatible
+	github.com/redis/go-redis/v9 v9.0.2
+	github.com/ugorji/go/codec v1.2.8
+	golang.org/x/crypto v0.5.0
+	golang.org/x/net v0.5.0
+	golang.org/x/sys v0.4.0
+	golang.org/x/text v0.6.0
+	google.golang.org/protobuf v1.28.1
+	gopkg.in/yaml.v2 v2.4.0
+  github.com/joho/godotenv v1.5.1
+)
---
+++ server/src/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+)
+
+func loadEnv() {
+	envFile := ".env"
+
+	_, err := os.Stat(envFile)
+	if err != nil {
+		return
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func createRedisClients() (*redis.Client, *redis.Client, *redis.Client) {
+	redisAddress := os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	redisMovieDatabase, _ := strconv.Atoi(os.Getenv("REDIS_MOVIE_DATABASE"))
+	redisMovieMainActorsDatabase, _ := strconv.Atoi(os.Getenv("REDIS_MOVIE_MAIN_ACTORS_DATABASE"))
+	redisActorDatabase, _ := strconv.Atoi(os.Getenv("REDIS_ACTOR_DATABASE"))
+
+	movieClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: redisPassword,
+		DB:       redisMovieDatabase,
+	})
+
+	movieMainActorClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: redisPassword,
+		DB:       redisMovieMainActorsDatabase,
+	})
+
+	actorClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: redisPassword,
+		DB:       redisActorDatabase,
+	})
+
+	return movieClient, movieMainActorClient, actorClient
+}
+
+func sendStringifiedEntity(stringifiedEntity string, context *gin.Context) {
+	if len(stringifiedEntity) > 0 {
+		context.JSON(http.StatusOK, stringifiedEntity)
+	} else {
+		context.JSON(http.StatusNotFound, nil)
+	}
+}
+
+func main() {
+	loadEnv()
+
+	server := gin.Default()
+	mainContext := context.Background()
+	movieClient, movieMainActorClient, actorClient := createRedisClients()
+
+	server.GET("/movies/:movieId", func(context *gin.Context) {
+		movieId := context.Param("movieId")
+		stringifiedMovie, _ := movieClient.Get(mainContext, movieId).Result()
+		sendStringifiedEntity(stringifiedMovie, context)
+	})
+
+	server.GET("/movie-main-actors/:movieId", func(context *gin.Context) {
+		movieId := context.Param("movieId")
+		stringifiedMovieMainActors, _ := movieMainActorClient.Get(mainContext, movieId).Result()
+		sendStringifiedEntity(stringifiedMovieMainActors, context)
+	})
+
+	server.GET("/actors/:actorId", func(context *gin.Context) {
+		actorId := context.Param("actorId")
+		stringifiedActor, _ := actorClient.Get(mainContext, actorId).Result()
+		sendStringifiedEntity(stringifiedActor, context)
+	})
+
+	port := os.Getenv("PORT")
+	server.Run(":" + port)
+}
